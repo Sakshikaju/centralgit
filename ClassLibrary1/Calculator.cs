e4fdf2339a4af53ab1ee22ab3f4166715a15613c
Sakshikaju <sakshikothary@gmail.com>
2022-10-11 15:09:53 +0530

    My third commit

---
+++ ClassLibrary1/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+
+        public int Substract(int a, int b)
+        {
+            return a - b;
+
+        }
+
+    }
+}
